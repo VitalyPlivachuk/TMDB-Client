3f30b9dda9bd4f3e8ce6497cfafa21b915de5528
VitalyPlivachuk <vitaly.plivachuk@gmail.com>
2017-06-26 21:59:52 +0300

    Initial Commit

---
+++ TMDB Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TMDB Client
+//
+//  Created by Vitaly Plivachuk on 26.06.17.
+//  Copyright © 2017 Vitaly Plivachuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
